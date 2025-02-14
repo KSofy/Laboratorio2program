715c69777af82de098e6a86932941e179ac74c46
Kathya Sofia Melgar Marroquin <kmelgarm2@miumg.edu.gt>
2025-02-14 16:07:16 -0600

    commit 2
    parte 2

---
+++ operacionesmatematicas/operacionesmatematicas/Program.cs
@@ -0,0 +1,13 @@
+﻿int num1, num2;
+
+Console.Write("Ingresa el primer numero (int): ");
+num1 = int.Parse(Console.ReadLine());
+
+Console.Write("Ingresa el segundo numero (int): ");
+num2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Suma: {num1 + num2}");
+Console.WriteLine($"Resta: {num1 - num2}");
+Console.WriteLine($"Multiplicacion: {num1 * num2}");
+Console.WriteLine($"Division: {num1 / num2}");
+
